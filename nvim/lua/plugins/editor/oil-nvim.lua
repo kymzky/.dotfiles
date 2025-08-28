a4955c1d5bf742274864e0fe2f7c0a36dc12b46c
kymzky <kymzky@outlook.com>
2025-08-28 16:11:10 +0200

    Add oil-nvim keymap

--- nvim/lua/plugins/editor/oil-nvim.lua
+++ nvim/lua/plugins/editor/oil-nvim.lua
@@ -6,5 +6,20 @@ return {
 		view_options = {
 			show_hidden = true,
 		},
+		keymaps = {
+			['<CR>'] = function()
+				local oil = require('oil')
+				local actions = require('oil.actions')
+				local entry = oil.get_cursor_entry()
+				vim.notify(entry.type)
+				if entry and entry.type == 'file' then
+					local path = oil.get_current_dir() .. entry.name
+					vim.cmd('bdelete')
+					vim.cmd('edit ' .. vim.fn.fnameescape(path))
+				else
+					actions.select.callback()
+				end
+			end,
+		},
 	},
 }
