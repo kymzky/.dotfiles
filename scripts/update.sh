b3dce8f731191d85f76ab58838cac79d540ae7b2
Daniel Ellert <daniel.ellert@secunet.com>
2026-02-10 08:35:21 +0100

    Add apt to update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+if command -v apt &> /dev/null; then
+  sudo apt update && sudo apt upgrade -y
+fi
+
 if command -v dnf &> /dev/null; then
   sudo dnf upgrade -y
 fi
 
 if command -v brew &> /dev/null; then
-  brew update
+  brew update && brew upgrade
 fi
